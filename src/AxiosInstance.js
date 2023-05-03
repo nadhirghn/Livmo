c8a405f653bc306a97835b0cfd663c8fade6e68b
rimKhedher2022 <rimkhedher0@gmail.com>
2023-05-03 11:39:17 -0700

    fixing user state and thumb image

---
+++ src/AxiosInstance.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const axiosInstance = axios.create({
+    // baseURL: 'https://backendlivmo.onrender.com/api/v1,
+    baseURL: 'http://localhost:3000/api/v1',
+    withCredentials: true,
+
+});
+
+module.exports = axiosInstance;
